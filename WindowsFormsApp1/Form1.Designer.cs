02f9c6ed45c15f7497ac1f752729be8fc60df4de
artem popkov <popkov.ad@icloud.com>
2024-02-01 22:48:38 +0300

    aaaaДобавьте файлы проекта.sa

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -31,15 +31,14 @@ namespace WindowsFormsApp1
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.time = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.duration = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.task = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.completion = new System.Windows.Forms.DataGridViewCheckBoxColumn();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,6 +56,36 @@ namespace WindowsFormsApp1
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(1026, 488);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(329, 29);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dateTimePicker1.Size = new System.Drawing.Size(433, 49);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(12, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 49);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(888, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 49);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Date
             // 
@@ -70,7 +99,6 @@ namespace WindowsFormsApp1
             this.time.FillWeight = 10F;
             this.time.HeaderText = "Время";
             this.time.Name = "time";
-            this.time.ReadOnly = true;
             // 
             // duration
             // 
@@ -90,51 +118,11 @@ namespace WindowsFormsApp1
             this.completion.HeaderText = "Выполнение";
             this.completion.Name = "completion";
             // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.dateTimePicker1.Location = new System.Drawing.Point(329, 29);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.dateTimePicker1.Size = new System.Drawing.Size(433, 49);
-            this.dateTimePicker1.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 83);
-            this.button1.TabIndex = 2;
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.Location = new System.Drawing.Point(888, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(150, 83);
-            this.button2.TabIndex = 3;
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(792, 42);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1050, 601);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dateTimePicker1);
@@ -150,8 +138,6 @@ namespace WindowsFormsApp1
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
@@ -160,7 +146,7 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.DataGridViewTextBoxColumn duration;
         private System.Windows.Forms.DataGridViewTextBoxColumn task;
         private System.Windows.Forms.DataGridViewCheckBoxColumn completion;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,14 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
 using System.IO;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
@@ -32,6 +26,7 @@ namespace WindowsFormsApp1
             dateTimePicker1.Value = dateTimePicker1.Value.AddDays(-1);
             data_load();
         }
+
         private void Form1_Load(object sender, EventArgs e)
         {
             data_load();
@@ -39,13 +34,38 @@ namespace WindowsFormsApp1
 
         private void data_load()
         {
-            dataGridView1.Rows.Clear();
-            for (int i = 0; i < 13; i++)
+            string file = Application.StartupPath + "\\log\\" + dateTimePicker1.Value.ToString("dd.MM.yy") + ".txt";
+            DateTime t = DateTime.Parse("08:00");
+            if (File.Exists(file))
             {
-                dataGridView1.Rows.Add();
-                dataGridView1.Rows[i].Cells[0].Value = dateTimePicker1.Value.ToString("dd.MM.yy");
-                dataGridView1.Rows[i].Cells[1].Value = (i + 8) + ":00";
-                dataGridView1.Rows[i].Cells[2].Value = 1;
+                StreamReader sr = new StreamReader(file);
+                string s;
+                dataGridView1.Rows.Clear();
+                for (int i = 0; i < 13; i++)
+                {
+                    s = sr.ReadLine();
+                    dataGridView1.Rows.Add();
+                    dataGridView1.Rows[i].Cells[0].Value = dateTimePicker1.Value.ToString("dd.MM.yy");
+                    dataGridView1.Rows[i].Cells[1].Value = s.Split('\\')[1];
+                    dataGridView1.Rows[i].Cells[2].Value = s.Split('\\')[2];
+                    dataGridView1.Rows[i].Cells[3].Value = s.Split('\\')[3];
+                    dataGridView1.Rows[i].Cells[4].Value = Convert.ToBoolean(s.Split('\\')[4]);
+                }
+                sr.Close();
+            }
+            else
+            {
+                dataGridView1.Rows.Clear();
+                for (int i = 0; i < 13; i++)
+                {
+                    dataGridView1.Rows.Add();
+                    dataGridView1.Rows[i].Cells[0].Value = dateTimePicker1.Value.ToString("dd.MM.yy");
+                    dataGridView1.Rows[i].Cells[1].Value = t.AddMinutes(i * 60).ToString("HH:mm");
+                    dataGridView1.Rows[i].Cells[2].Value = 1;
+                    dataGridView1.Rows[i].Cells[3].Value = string.Empty;
+                    dataGridView1.Rows[i].Cells[3].Value = string.Empty;
+                    dataGridView1.Rows[i].Cells[4].Value = false;
+                }
             }
         }
 
@@ -60,13 +80,13 @@ namespace WindowsFormsApp1
             sw.Close();
         }
 
-        private void load()
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
         {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
+            for (int i = e.RowIndex + 1; i < 13; i++)
+            {
+                DateTime t = DateTime.Parse(dataGridView1.Rows[i - 1].Cells["time"].Value.ToString());
+                dataGridView1.Rows[i].Cells["time"].Value = t.AddMinutes(Convert.ToDouble(dataGridView1.Rows[i - 1].Cells["duration"].Value) * 60).ToString("HH:mm");
+            }
             save();
         }
     }
